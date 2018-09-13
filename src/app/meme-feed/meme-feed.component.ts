0ff8d7ee56cfb993832a5e0b6e3793bdf3792730
Fabian Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2018-09-13 11:20:44 -0300

    Firebase lists y vista de eso

--- src/app/meme-feed/meme-feed.component.ts
+++ src/app/meme-feed/meme-feed.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-meme-feed',
@@ -6,20 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meme-feed.component.css']
 })
 export class MemeFeedComponent implements OnInit {
-  memes = [
-    { 
-      title : "6 sentido JS",
-      image : "https://statics.memondo.com/p/99/ccs/2016/05/CC_2581711_3279a685ee0247abb69433addae1a7b1_meme_otros_solo_programadores_xd.jpg?cb=6607706",
-      description : "Cuando no declaras tus variables..."
-    },
-    {
-      title : "Borrachera de código",
-      image : "https://4.bp.blogspot.com/-25kufFKVqWg/Vxet39_4TkI/AAAAAAAAAvc/OzdiQYqKRCAtEYaOCPq1xuriD90SRKZ4ACLcB/s640/12524173_1084250148303422_7360188813809398316_n.jpg",
-      description : "Cuando te contratan sabiendo como programas"
-    }
-  ];
+  memes$:Observable<any>; 
 
-  constructor() { }
+  constructor(private database:AngularFireDatabase) {
+    this.memes$ = this.database.list('/memes').valueChanges(); // esto es como el once, se va actualizando todo
+  }
 
   ngOnInit() {
   }
